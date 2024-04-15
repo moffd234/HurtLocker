08ab48b45ef9a8696894a3657349496f8dec3470
moffd234 <moffd234@gmail.com>
2024-04-15 17:15:39 -0400

    Kinda able to output the data in the format desired

--- src/main/java/JerksonParser.java
+++ src/main/java/JerksonParser.java
@@ -1,6 +1,11 @@
 import org.apache.commons.io.IOUtils;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JerksonParser {
+
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -11,7 +16,11 @@ public class JerksonParser {
     public static void main(String[] args) throws Exception{
         JerksonParser jerkson = new JerksonParser();
         String output = (new JerksonParser()).readRawDataToString();
-        jerkson.format();
+        String[] objects = jerkson.format();
+        for (int i = 0; i < objects.length - 1; i++) {
+            String obj = objects[i];
+            System.out.println(jerkson.prettyFormat(obj, objects));
+        }
     }
 
     public String[] format() {
@@ -47,15 +56,38 @@ public class JerksonParser {
             }
             sb.append("; ");
         }
+
         return sb.toString();
     }
 
     public String prettify(String[] objects) {
-        String output = "";
-        String[] items = new String[5];
-
-
         return null;
+    }
+
+    public String prettyFormat(String jerkSONObject, String[] objects){
+        int errorCount = 0;
+        String[] keyValuePairs = jerkSONObject.split("; ");
+        String[] values = new String[4];
+        String output = "";
+        for (int i = 0; i < keyValuePairs.length; i++) {
+            String pairs = keyValuePairs[i];
+            try {
+                String value = pairs.split(": ")[1];
+                values[i] = value;
+                String name = values[0];
+                int count = countOccurrences(objects, name);
+                String price = values[1];
+                String type = values[2];
+                String date = values[3];
+                output = String.format("Name: %s\t\tseen: %s times\n=============\t" +
+                        "\t=============\nprice %s\t\tseen %s times\n" +
+                        "-------------\t\t-------------\n\n", name, count, price, count);
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                errorCount++;
+            }
+        }
+        return output;
     }
 
     public boolean alreadyAdded(String strToAdd, String[] strArray){
@@ -66,6 +98,7 @@ public class JerksonParser {
         }
         return false;
     }
+
     public int countOccurrences(String[] objects, String input) {
         int count = 0;
 
