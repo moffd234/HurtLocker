a16c741310904975173a7d05a0eeaf7c50886b96
moffd234 <moffd234@gmail.com>
2024-04-15 12:13:45 -0400

    Got the jerkson string to a readable format

--- src/main/java/JerksonParser.java
+++ src/main/java/JerksonParser.java
@@ -11,20 +11,50 @@ public class JerksonParser {
 
 
     public static void main(String[] args) throws Exception{
+        JerksonParser jerkson = new JerksonParser();
         String output = (new JerksonParser()).readRawDataToString();
-        System.out.println(output);
-
+        jerkson.format();
     }
 
-    public String format() {
-        String separators = ":@^*%";
-        return null;
+    public String[] format() {
+        String fieldRegex = "[!@;^%*]";
+        String rawData = null;
+        try {
+            rawData = readRawDataToString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String[] objects = separateObjects(rawData);
+        for(int i = 0; i < objects.length; i++){
+            objects[i] = processFields(objects[i]);
+        }
+        return objects;
     }
     public String[] separateObjects(String rawData){
-        String fieldRegex = "[!@;^%*]";
         String objectRegex = "##";
-        String kvRegex = "[:@^*%]";
-        System.out.println(Arrays.toString(rawData.split("##")));
-        return rawData.split("##");
+        return rawData.split(objectRegex);
+    }
+
+    public String processFields(String object) {
+
+        String kvRegex = "[:@^*%;!]";
+        String[] kvPairs = object.split(kvRegex); // Split the KV pairs
+        StringBuilder sb = new StringBuilder();
+
+        // Fix Jerkson so it actually looks normal and readable
+        for (int i = 0; i < kvPairs.length - 1; i += 2) {
+            sb.append(kvPairs[i]);
+            sb.append(": ");
+            if (i + 1 < kvPairs.length) {
+                sb.append(kvPairs[i + 1]);
+            }
+            sb.append("; ");
+        }
+        System.out.println(sb);
+        return sb.toString();
+    }
+
+    public String getOutput() {
+        return null;
     }
 }
--- src/test/java/JerkSONTest.java
+++ src/test/java/JerkSONTest.java
@@ -19,7 +19,7 @@ public class JerkSONTest {
     @Test
     public void outputTests() {
         String expected =
-                "name:    Milk \t\t seen: 6 times\n" +
+                "name:    Milk \t\t seen: 6 times\n"+
                 "============= \t \t =============\n" +
                 "Price: \t 3.23\t\t seen: 5 times\n" +
                 "-------------\t\t -------------\n" +
@@ -43,7 +43,7 @@ public class JerkSONTest {
                 "\n" +
                 "Errors         \t \t seen: 4 times\n";
 
-        String actual = jerkson.format();
+        String actual = jerkson.getOutput();
         Assert.assertEquals(expected, actual);
     }
 
@@ -76,6 +76,25 @@ public class JerkSONTest {
     public void testObjectSplit(){
         String[] expected = {"naMe:Milk;price:3.23;type:Food;expiration:1/25/2016", "naME:BreaD;price:1.23;type:Food;expiration:1/02/2016", "NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016", "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016", "naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016", "naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016", "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016", "naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016", "NAME:MilK;price:3.23;type:Food;expiration:1/17/2016", "naMe:MilK;price:1.23;type:Food!expiration:4/25/2016", "naMe:apPles;price:0.25;type:Food;expiration:1/23/2016", "naMe:apPles;price:0.23;type:Food;expiration:5/02/2016", "NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016", "naMe:;price:3.23;type:Food;expiration:1/04/2016", "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016", "naME:BreaD;price:1.23;type:Food@expiration:1/02/2016", "NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016", "naMe:MiLK;priCe:;type:Food;expiration:1/11/2016", "naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016", "naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016", "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016", "naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016", "NAME:MilK;price:3.23;type:Food;expiration:1/17/2016", "naMe:MilK;priCe:;type:Food;expiration:4/25/2016", "naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016", "naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016", "NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016", "naMe:;price:3.23;type:Food^expiration:1/04/2016"};
         String[] actual = jerkson.separateObjects(rawData);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void format() {
+        String[] expected = {"naMe: Milk; price: 3.23; type: Food; expiration: 1/25/2016; ", "naME: BreaD; price: 1.23; type: Food; expiration: 1/02/2016; ", "NAMe: BrEAD; price: 1.23; type: Food; expiration: 2/25/2016; ", "naMe: MiLK; price: 3.23; type: Food; expiration: 1/11/2016; ", "naMe: Cookies; price: 2.25; type: Food; expiration: 1/25/2016; ", "naMe: CoOkieS; price: 2.25; type: Food; expiration: 1/25/2016; ","naMe: COokIes; price: 2.25; type: Food; expiration: 3/22/2016; ","naMe: COOkieS; price: 2.25; type: Food; expiration: 1/25/2016; ","NAME: MilK; price: 3.23; type: Food; expiration: 1/17/2016; ","naMe: MilK; price: 1.23; type: Food; expiration: 4/25/2016; ","naMe: apPles; price: 0.25; type: Food; expiration: 1/23/2016; ","naMe: apPles; price: 0.23; type: Food; expiration: 5/02/2016; ","NAMe: BrEAD; price: 1.23; type: Food; expiration: 1/25/2016; ","naMe: ; price: 3.23; type: Food; expiration: 1/04/2016; ","naMe: Milk; price: 3.23; type: Food; expiration: 1/25/2016; ","naME: BreaD; price: 1.23; type: Food; expiration: 1/02/2016; ","NAMe: BrEAD; price: 1.23; type: Food; expiration: 2/25/2016; ","naMe: MiLK; priCe: ; type: Food; expiration: 1/11/2016; ","naMe: Cookies; price: 2.25; type: Food; expiration: 1/25/2016; ","naMe: Co0kieS; pRice: 2.25; type: Food; expiration: 1/25/2016; ","naMe: COokIes; price: 2.25; type: Food; expiration: 3/22/2016; ","naMe: COOkieS; Price: 2.25; type: Food; expiration: 1/25/2016; ","NAME: MilK; price: 3.23; type: Food; expiration: 1/17/2016; ","naMe: MilK; priCe: ; type: Food; expiration: 4/25/2016; ","naMe: apPles; prIce: 0.25; type: Food; expiration: 1/23/2016; ","naMe: apPles; pRice: 0.23; type: Food; expiration: 5/02/2016; ","NAMe: BrEAD; price: 1.23; type: Food; expiration: 1/25/2016; ","naMe: ; price: 3.23; type: Food; expiration: 1/04/2016; "};
+
+        String[] actual = jerkson.format();
+        Assert.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testFieldProcessor() {
+        String expected = "naMe: Milk; price: 3.23; type: Food; expiration: 1/25/2016; ";
+
+        String[] objects = jerkson.separateObjects(rawData);
+        String actual = jerkson.processFields(objects[0]);
+
         Assert.assertEquals(expected, actual);
     }
 }
